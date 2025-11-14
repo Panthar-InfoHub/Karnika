83ec547ff4141e9560bd690272289c7b4b4eb4ac
Shiva Yadav <shivaydv1256@gmail.com>
2025-11-14 11:44:04 +0530

    fixed eslint error

--- app/(store)/checkout/page.tsx
+++ app/(store)/checkout/page.tsx
@@ -1,25 +1,7 @@
 import { getAddresses } from "@/actions/store/address.actions";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import dynamic from "next/dynamic";
-import { Skeleton } from "@/components/ui/skeleton";
-
-// Lazy load CheckoutForm (heavy component with Razorpay)
-const CheckoutForm = dynamic(
-  () =>
-    import("@/components/store/checkout/checkout-form").then((mod) => ({
-      default: mod.CheckoutForm,
-    })),
-  {
-    loading: () => (
-      <div className="space-y-6">
-        <Skeleton className="h-[400px] w-full" />
-        <Skeleton className="h-[200px] w-full" />
-      </div>
-    ),
-    ssr: false,
-  }
-);
+import { CheckoutClient } from "./checkout-client";
 
 export default async function CheckoutPage() {
   // Get session on server
@@ -35,7 +17,7 @@ export default async function CheckoutPage() {
   return (
     <div className="min-h-screen py-8">
       <div className="custom-container">
-        <CheckoutForm userEmail={session?.user?.email} savedAddresses={savedAddresses} />
+        <CheckoutClient userEmail={session?.user?.email} savedAddresses={savedAddresses} />
       </div>
     </div>
   );
